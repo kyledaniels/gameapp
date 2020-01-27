87aadf5e5b88978982e0f4a686cd690ca0486eab
kyle67 <daniels.kyle@gmail.com>
2020-01-27 12:11:53 -0500

    Modified Home Page

---
+++ gametestapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Home from './components/Home';
+
+function App() {
+  return (
+    <Router>
+
+      <Route path="/" component={Home}></Route>
+    </Router>
+  );
+}
+
+export default App;
